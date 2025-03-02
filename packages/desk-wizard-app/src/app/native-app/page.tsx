e7bf6875f16790faef4b5f6344078ffb4cb4f7fe
Marco Bischoff <bischoff_m@aim.com>
2025-03-02 01:30:21 +0100

    Fix next.js <Link> instead of <a>

--- packages/desk-wizard-app/src/app/native-app/page.tsx
+++ packages/desk-wizard-app/src/app/native-app/page.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
         <>
             <div className="flex w-full h-full absolute overflow-hidden">
                 <div className="flex justify-center items-center w-full h-full bg-red-800">
-                    <a href="/" className="text-2xl font-bold text-center">
+                    <Link href="/" className="text-2xl font-bold text-center">
                         Send back
-                    </a>
+                    </Link>
                 </div>
             </div>
         </>
